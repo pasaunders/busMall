4d1f5904e00c6de17d2c07fe1212949b9db1e378
pasaunders <kaorti@gmail.com>
2016-01-19 15:32:01 -0800

    got the images showing, midway through the event listneners.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var productList = [];
+var randomProductsArray = []; //temporarily holds the three images until they are displayed.
+var clickCounter = 0;
 
 function Product(picturePath){
   this.picturePath = picturePath;
@@ -26,19 +28,20 @@ var usb = new Product('img/usb.gif');
 var waterCan = new Product('img/watercan.jpg');
 var wineGlass = new Product('img/wineglass.jpg');
 
-var thisSelection = [];
 function randomProducts() {
   console.log('randomizer function');
   var productGenerated = 0;
-  while (thisSelection.length < 3) {
+  while (randomProductsArray.length < 3) {
     console.log('first while loop');
-    // do {
+    //start nested loop
+    do {
       console.log('do-while loop');
       productGenerated = Math.floor(Math.random() * productList.length);
       console.log('Number generated: ' + productGenerated);
-    // } while (thisSelection.indexOf(productGenerated) === -1 false);
-    thisSelection.push(productGenerated);
-    console.log('Array contains: ' + thisSelection);
+    } while (randomProductsArray.indexOf(productGenerated) !== -1);
+
+    randomProductsArray.push(productGenerated);
+    console.log('Array contains: ' + randomProductsArray);
   }
 }
 
@@ -47,17 +50,52 @@ function displayProducts() {
   console.log('display loop running');
   randomProducts();
 
-  for (var i = 0; i < thisSelection.length; i++) {
-    var pictureLocation = document.getElementById('picture');
+  for (var i = 0; i < randomProductsArray.length; i++) {
+    console.log('inside for loop inside displayProducts');
+    var imageSpots = ['pictureOne', 'pictureTwo', 'pictureThree'];
+    console.log('imageSpots = ' + imageSpots);
+    var pictureLocation = document.getElementById(imageSpots[i]);
+    console.log('pictureLocation= ' + pictureLocation);
+    console.log('imageSpots[i]= ' + imageSpots[i]);
     var createImage = document.createElement('img');
-    var attachImage = document.setAttribute('src', thisRound[i].picturePath);
-    createElement.appendChild(attachImage);
-    pictureLocation.appendChild(createElement);
+    createImage.src = productList[randomProductsArray[i]].picturePath;
+    pictureLocation.appendChild(createImage);
+//stick code to track times shown here
+
   }
 }
+
 displayProducts();
 
-// for (var i = 0; i < 3; i++) {
-//   displayProducts();
-//   console.log('display loop is running');
-// }
+// event handling
+pictureOne.addEventListener('click', productOneChosen);
+pictureTwo.addEventListener('click', productTwoChosen);
+pictureThree.addEventListener('click', productThreeChosen);
+
+function productOneChosen() {
+  console.log(event);
+  event.preventDefault();
+  productList[randomProductsArray[0]].votes +=
+  clickCounter++;
+  if (clickCounter < 15) {
+    displayProducts();
+  }
+}
+function productTwoChosen() {
+  console.log(event);
+  event.preventDefault();
+  productList[randomProductsArray[1]].votes +=
+  clickCounter++;
+  if (clickCounter < 15) {
+    displayProducts();
+  }
+}
+function productThreeChosen(){
+  console.log(event);
+  event.preventDefault();
+  productList[randomProductsArray[2]].votes +=
+  clickCounter++;
+  if (clickCounter < 15) {
+    displayProducts();
+  }
+}
